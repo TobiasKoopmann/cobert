279fc6ce938a81afa2b8f14e4cb20b13f842ff48
Tobias Koopmann <tobias.koopmann@uni-wuerzburg.de>
2022-05-02 09:50:37 +0200

    kubernetes removed

--- kubernetes/wandb/chown_skript.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for dir in /$1/*; do
-  echo "Try changing $dir"
-  chown -R 1147:1003 $dir || echo "Cannot own $dir"
-done
--- kubernetes/wandb/deploy_wandb.sh
+++
@@ -1,5 +0,0 @@
-docker build -t ls6-staff-registry.informatik.uni-wuerzburg.de/koopmann/lsx-wandb:latest .
-docker push ls6-staff-registry.informatik.uni-wuerzburg.de/koopmann/lsx-wandb:latest
-
-kubectl -n koopmann delete deployment wandb
-kubectl -n koopmann apply -f weightsbiases.yml
